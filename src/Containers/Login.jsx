5a71de327e408e2c378f5df80ed3484ae2250290
aditya132p <aditya132p@gmail.com>
2024-09-10 21:05:47 +0530

    favicon changes in html

--- src/Containers/Login.jsx
+++ src/Containers/Login.jsx
@@ -25,10 +25,12 @@ function Login() {
               <Text id="password" className='font-semibold'>Password</Text>
               {/* <input id="password" className='w-full border px-2 py-2 rounded-md border-gray-200' type="password" placeholder="Enter your password" required /> */}
               <Input className='w-full border px-2 outline-none py-2 rounded-md border-gray-200' type="password" placeholder="Enter your password" />
+              <Text className="">Forgot Password ?</Text>
             </div>
             <Button type="submit" className=" py-3 rounded-md w-full bg-black text-white hover:bg-gray-900">
               Sign in
             </Button>
+           
           </form>
         </div>
       </div> 
