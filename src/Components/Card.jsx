9987c4e6e8af5b0e9ea9e6203a7099648f79f20d
aditya132p <aditya132p@gmail.com>
2024-09-11 12:12:45 +0530

    Framer motion animation added to the components

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,23 @@
+import { motion } from "framer-motion"
 const Card = (props) => {
+  const transition = {
+    duration: 0.5,
+    ease: [0, 0.61, 0.1, 1.01],
+    scale: {
+      type: "spring",
+      damping: 10,
+      stiffness: 100,
+      restDelta: 0.0002
+    }
+  }
   return (
-      <div className="w-full max-w-md bg-white p-10 rounded-lg drop-shadow-sm ">{props.children}</div>
+    <motion.div
+      initial={{ opacity: 0, x: 600, }}
+      animate={{ opacity: 1, x: 0, }}
+      transition={transition}
+      className="w-full max-w-md bg-white p-10 rounded-lg drop-shadow-sm ">
+      {props.children}
+      </motion.div>
   )
 }
 
--- src/Containers/Login.jsx
+++ src/Containers/Login.jsx
@@ -4,6 +4,7 @@ import Img from "../Components/Img";
 import Input from "../Components/Input";
 import { Link } from "react-router-dom";
 import Card from "../Components/Card";
+// import { motion } from "framer-motion";
 
 
 function Login() {
@@ -14,7 +15,7 @@ function Login() {
        <Card>
           <div className="w-full max-w-md space-y-8">
             <div className='flex   items-center flex-col'>
-              <Img src="logo.png" className="w-fit" />
+              <Img src="logoog.svg" className="w-fit" />
               <Text className="mt-6  text-3xl font-bold tracking-tight text-foreground" >Sign in to your account</Text>
 
             </div>
@@ -22,7 +23,7 @@ function Login() {
               <div className='flex flex-col'>
                 <Text id="username" className='font-medium'>Username</Text>
                 {/* <input className='w-full border px-2 py-2 rounded-md border-gray-200' id="username" type="text" placeholder="Enter your username" required /> */}
-                <Input className='w-full border px-2 outline-none py-2 rounded-md border-gray-200' type="password" placeholder="Enter your password" />
+                <Input className='w-full border px-2 outline-none py-2 rounded-md border-gray-200' type="password" placeholder="Enter your Username" />
               </div>
               <div className='flex flex-col'>
                 <Text id="password" className='font-semibold'>Password</Text>
@@ -32,10 +33,10 @@ function Login() {
                   <Text id="password" className='font-semibold'> Forgot Password ?</Text>
                 </Link>
               </div>
+              
               <Button type="submit" className=" py-3 rounded-md w-full bg-black text-white hover:bg-gray-900">
                 Sign in
               </Button>
-
             </form>
           </div>
        </Card>
